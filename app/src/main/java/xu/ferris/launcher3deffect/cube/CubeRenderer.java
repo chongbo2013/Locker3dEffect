384050d1b414f512d7a0d0aa928a8536eb281f19
xufeifandj <459821731@qq.com>
2016-02-17 11:30:01 +0800

    修改水晶空翻效果

--- app/src/main/java/xu/ferris/launcher3deffect/cube/CubeRenderer.java
+++ app/src/main/java/xu/ferris/launcher3deffect/cube/CubeRenderer.java
@@ -18,7 +18,7 @@ import xu.ferris.launcher3deffect.R;
 
 public class CubeRenderer implements Renderer {
 
-    Bitmap bmp,sideBimap;
+    Bitmap bmp,sideBimap,fontBitmap;
 
     float box[] = new float[] {
             // FRONT
@@ -53,44 +53,8 @@ public class CubeRenderer implements Renderer {
             1f, -1f, -0.10f,
     };
 
-    float lightAmbient[] = new float[] { 0.5f, 0.5f, 0.6f, 1.0f };  //环境光
-    float lightDiffuse[] = new float[] { 0.6f, 0.6f, 0.6f, 1.0f };//漫反射光
-    float[] lightPos = new float[] {0,0,3,1};  //光源位置
-    /*
-     * 因为进行光照处理，你必须告知系统你定义的模型各个面的方向，以便系统计算光影情况，方向的描述是通过向量点来描述的
-     */
-    float norms[] = new float[] { //法向量数组，用于描述个顶点的方向，以此说明各个面的方向
-            // FRONT
-            0f,  0f,  1f, //方向为(0,0,0)至(0,0,1)即Z轴正方向
-            0f,  0f,  1f,
-            0f,  0f,  1f,
-            0f,  0f,  1f,
-            // BACK
-            0f,  0f,  -1f,
-            0f,  0f,  -1f,
-            0f,  0f,  -1f,
-            0f,  0f,  -1f,
-            // LEFT
-            -1f,  0f,  0f,
-            -1f,  0f,  0f,
-            -1f,  0f,  0f,
-            -1f,  0f,  0f,
-            // RIGHT
-            1f, 0f, 0f,
-            1f, 0f, 0f,
-            1f, 0f, 0f,
-            1f, 0f, 0f,
-            // TOP
-            0f,  1f, 0f,
-            0f,  1f, 0f,
-            0f,  1f, 0f,
-            0f,  1f, 0f,
-            // BOTTOM
-            0f,  -1f, 0f,
-            0f,  -1f, 0f,
-            0f,  -1f, 0f,
-            0f,  -1f, 0f
-    };
+
+
 
 
     float texCoords[] = new float[] { //纹理坐标对应数组
@@ -128,7 +92,6 @@ public class CubeRenderer implements Renderer {
 
 
     FloatBuffer cubeBuff;
-    FloatBuffer normBuff;
     FloatBuffer texBuff;
 
     float xrot = 0.0f;
@@ -151,83 +114,45 @@ public class CubeRenderer implements Renderer {
     public CubeRenderer(Context c) {
         // TODO Auto-generated constructor stub
         cubeBuff = makeFloatBuffer(box);//转换float数组
-        normBuff = makeFloatBuffer(norms);
         texBuff = makeFloatBuffer(texCoords);
-        bmp = BitmapFactory.decodeResource(c.getResources(), R.drawable.splash);
-        sideBimap= BitmapFactory.decodeResource(c.getResources(), R.drawable.test_1);
+        bmp = BitmapFactory.decodeResource(c.getResources(), R.drawable.theme_icons);//需要绘制的面
+        sideBimap= BitmapFactory.decodeResource(c.getResources(), R.drawable.gl_crystal_side);//侧面等
+        fontBitmap= BitmapFactory.decodeResource(c.getResources(), R.drawable.gl_crystal_font);//前面
     }
 
     private int[] textureids=null;
     protected void init(GL10 gl) {
-        gl.glClearColor(0,0,0,0);//设置清屏时背景的颜色，R，G，B，A
+        gl.glClearColor(0, 0,0,0);//设置清屏时背景的颜色，R，G，B，A
         gl.glEnable(GL10.GL_BLEND);
         gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-        gl.glEnable(GL10.GL_LIGHTING); //启用光照
-        gl.glEnable(GL10.GL_LIGHT0);  //开启光源0
-        //设置光照参数，也可以使用默认的，不设置
-        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, lightAmbient, 0);
-        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightDiffuse, 0);
-        gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, lightPos, 0);
-
-        gl.glNormalPointer(GL10.GL_FLOAT, 0, normBuff);
-        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
-
         //使用纹理步骤：
         // 1.开启贴图
         gl.glEnable(GL10.GL_TEXTURE_2D);
 
         // 2.生成纹理ID
-        textureids = new int[2];//尽管只有一个纹理，但使用一个元素的数组
+        textureids = new int[3];//尽管只有一个纹理，但使用一个元素的数组
         //glGenTextures(申请个数，存放数组，偏移值）
-        gl.glGenTextures(2, textureids, 0); //向系统申请可用的，用于标示纹理的ID
+        gl.glGenTextures(3, textureids, 0); //向系统申请可用的，用于标示纹理的ID
 
 
 
-        //3.绑定纹理，使得指定纹理处于活动状态。一次只能激活一个纹理
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[0]);
-        //4.绑定纹理数据，传入指定图片
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bmp, 0);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
 
 
-        gl.glTexParameterx(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MAG_FILTER,GL10.GL_LINEAR);
-        gl.glTexParameterx(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
-        gl.glEnable(GL10.GL_DEPTH_TEST); //启用深度缓存
-        gl.glEnable(GL10.GL_CULL_FACE);  //启用背面剪裁
-        gl.glClearDepthf(1.0f);    // 设置深度缓存值
-        gl.glDepthFunc(GL10.GL_LEQUAL);  // 设置深度缓存比较函数，GL_LEQUAL表示新的像素的深度缓存值小于等于当前像素的深度缓存值（通过gl.glClearDepthf(1.0f)设置）时通过深度测试
-        gl.glShadeModel(GL10.GL_SMOOTH);// 设置阴影模式GL_SMOOTH
-
-
-        //3.绑定纹理，使得指定纹理处于活动状态。一次只能激活一个纹理
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[1]);
-        //4.绑定纹理数据，传入指定图片
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, sideBimap, 0);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER,GL10.GL_LINEAR);
 
 
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[2]);
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, fontBitmap, 0);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
+        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER,GL10.GL_LINEAR);
 
-
-
-
-
-        //5.传递各个顶点对应的纹理坐标
-//        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texBuff);
-//        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY); //开启纹理坐标数组
-
-        //6.设置纹理参数 （可选）
-        /*下面的两行参数告诉OpenGL在显示图像时，当它比放大得原始的纹理大
-        *( GL_TEXTURE_MAG_FILTER )或缩小得比原始得纹理小( GL_TEXTURE_MIN_FILTER )
-        *时OpenGL采用的滤波方式。通常这两种情况下我都采用 GL_LINEAR 。这使得纹理从很远处
-        *到离屏幕很近时都平滑显示。使用 GL_LINEAR 需要CPU和显卡做更多的运算。如果您的机器很慢，
-        *您也许应该采用 GL_NEAREST 。过滤的纹理在放大的时候，看起来马赛克的很。您也可以结合这
-        *两种滤波方式。在近处时使用 GL_LINEAR ，远处时 GL_NEAREST 。
-        **/
-        gl.glTexParameterx(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MAG_FILTER,GL10.GL_LINEAR);
-        gl.glTexParameterx(GL10.GL_TEXTURE_2D,GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
-        gl.glEnable(GL10.GL_DEPTH_TEST); //启用深度缓存
-        gl.glEnable(GL10.GL_CULL_FACE);  //启用背面剪裁
-        gl.glClearDepthf(1.0f);    // 设置深度缓存值
-        gl.glDepthFunc(GL10.GL_LEQUAL);  // 设置深度缓存比较函数，GL_LEQUAL表示新的像素的深度缓存值小于等于当前像素的深度缓存值（通过gl.glClearDepthf(1.0f)设置）时通过深度测试
-        gl.glShadeModel(GL10.GL_SMOOTH);// 设置阴影模式GL_SMOOTH
     }
 
     @Override
@@ -249,28 +174,37 @@ public class CubeRenderer implements Renderer {
     public void onDrawFrame(GL10 gl) {
         // TODO Auto-generated method stub
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);// 清除屏幕和深度缓存
-
         gl.glMatrixMode(GL10.GL_MODELVIEW);   //切换至模型观察矩阵
         gl.glLoadIdentity();// 重置当前的模型观察矩阵
         GLU.gluLookAt(gl, 0, 0, 3, 0, 0, 0, 0, -1, 0);//设置视点和模型中心位置
 
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, cubeBuff);//设置顶点数据
+
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
         gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);// 使用纹理数组
-       gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texBuff);
+
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, cubeBuff);//设置顶点数据
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, texBuff);
 
         gl.glRotatef(yrot, 0, 1, 0);
 
 
 
-        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[0]);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[2]);
         gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);  //绘制正方型FRONT面
 
-        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[1]);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[0]);
         gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+
+
+
+
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[2]);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 4, 4);
 
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[0]);
         gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 4, 4);
+
+
 
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureids[1]);
         gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 8, 4);
